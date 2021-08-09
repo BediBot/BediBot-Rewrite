2c7b5e3312b99e1f90ec6b88a5723e3672faecd9
Zayd Tahir <zaydtahir20@gmail.com>
2021-08-09 00:18:52 -0400

    Setting up Initial Bot

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import {LogLevel, SapphireClient} from '@sapphire/framework';
+
+require('dotenv').config();
+
+const client = new SapphireClient({
+  defaultPrefix: '$',
+  caseInsensitiveCommands: true,
+  logger: {
+    level: LogLevel.Trace,
+  },
+});
+
+const main = async () => {
+  try {
+    client.logger.info('Bot is logging in');
+    await client.login(process.env.BOT_TOKEN);
+    client.logger.info('Bot has logged in');
+  } catch (error) {
+    client.logger.fatal(error);
+    client.destroy();
+    process.exit(1);
+  }
+};
+
+main().catch(console.error);
