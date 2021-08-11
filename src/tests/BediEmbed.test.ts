f0c9621ccaa95540099127fda9cd0f4a3a3e235b
Zayd Tahir <zaydtahir20@gmail.com>
2021-08-11 08:33:19 -0400

    Generate Testing Report (#26)

--- src/tests/BediEmbed.test.ts
+++ src/tests/BediEmbed.test.ts
@@ -1,17 +1,19 @@
 import {BediEmbed} from '../lib/BediEmbed';
 
-test('BediEmbed has Correct Footer', () => {
-  const {footer} = new BediEmbed();
-  const now = Date.now();
+describe('BediEmbed', () => {
+  test('BediEmbed has Correct Footer', () => {
+    const {footer} = new BediEmbed();
+    const now = Date.now();
 
-  expect(typeof footer).toBe('object');
-  expect(footer!.text).toBe('For any concerns, contact a BediBot dev: Aadi, Carson, Joe, Sahil, & Zayd');
-});
+    expect(typeof footer).toBe('object');
+    expect(footer!.text).toBe('For any concerns, contact a BediBot dev: Aadi, Carson, Joe, Sahil, & Zayd');
+  });
 
-test('BediEmbed has Correct Timestamp', () => {
-  const {timestamp} = new BediEmbed();
+  test('BediEmbed has Correct Timestamp', () => {
+    const {timestamp} = new BediEmbed();
 
-  const now = Date.now();
+    const now = Date.now();
 
-  expect(timestamp).toBe(now);
+    expect(timestamp).toBe(now);
+  });
 });
