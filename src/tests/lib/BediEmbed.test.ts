f532310a277fbefa198f4db6c9f83fd8f262ddb5
Zayd Tahir <zaydtahir20@gmail.com>
2021-08-12 22:34:46 -0400

    Update test

--- src/tests/lib/BediEmbed.test.ts
+++ src/tests/lib/BediEmbed.test.ts
@@ -5,7 +5,7 @@ describe('BediEmbed', () => {
     const {footer, color, timestamp} = new BediEmbed();
     const now = Date.now();
 
-    expect(timestamp).toBeCloseTo(now);
+    expect(timestamp).toBe(now);
     expect(color).toBe(3553599);
     expect(typeof footer).toBe('object');
     expect(footer!.text).toBe('For any concerns, contact a BediBot dev: Aadi, Carson, Joe, Sahil, & Zayd');
