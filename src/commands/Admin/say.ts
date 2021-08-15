421bd00d7e2a6ea785e01094ad8a8eed0f171163
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-08-15 14:08:23 -0400

    Say command (#59)
    * Get working without channel arg
    
    * Command works!
    
    * Address Zayd's comments

---
+++ src/commands/Admin/say.ts
@@ -0,0 +1,70 @@
+import {Args, PieceContext} from '@sapphire/framework';
+import {Message} from 'discord.js';
+import {getSettings} from '../../database/models/SettingsModel';
+import {BediEmbed} from '../../lib/BediEmbed';
+import colors from '../../utils/colorUtil';
+import {purge_messages} from '../../utils/discordUtil';
+
+const MAX_MSGS_THAT_CAN_BE_DELETED = 100;
+
+const {Command} = require('@sapphire/framework');
+
+module.exports = class SayCommand extends Command {
+  constructor(context: PieceContext) {
+    super(context, {
+      name: 'say',
+      description: 'Sends a message from the bot',
+      preconditions: [['AdminOnly', 'BotOwnerOnly'], 'GuildOnly'],
+    });
+  }
+
+  async run(message: Message, args: Args) {
+    const {guild, guildId, author} = message;
+    const settingsData = await getSettings(guildId as string);
+
+    //Pick the title and content from args, return error if invalid
+    const say_title = await args.pickResult('string');
+    const say_content = await args.pickResult('string');
+    if (!say_title.success || !say_content.success) {
+      const embed = new BediEmbed()
+          .setColor(colors.ERROR)
+          .setTitle('Say Reply')
+          .setDescription('Invalid Syntax!\n\nMake sure your command is in the format `' + settingsData.prefix + 'say <title> <body> (channel)`');
+      return message.reply({embeds: [embed]});
+    }
+
+    //Parse channel args
+    let channel = message.channel;
+    if(!args.finished)
+    {
+        const arg_channel = await args.pickResult('guildTextChannel');
+
+        if (!arg_channel.success) {
+            const embed = new BediEmbed()
+                .setColor(colors.ERROR)
+                .setTitle('Say Reply')
+                .setDescription('Invalid Syntax!\n\nMake sure your command is run in the format `' + settingsData.prefix + 'say <title> <body> (channel)`');
+            return message.reply({embeds: [embed]});
+          }
+         
+        channel = arg_channel.value;
+    }
+
+    let body_content_to_send = say_content.value
+    const BOT_OWNERS = process.env.BOT_OWNERS!.split(',');
+    if(!BOT_OWNERS.includes(message.author.id))
+    {
+        //Append the user's @ to the message so that $say messages aren't mistaken for actual bot messages
+        body_content_to_send = body_content_to_send.concat("\n\nMessage created by " + message.author);
+    }
+    
+    //Delete the original message
+    message.delete();
+
+    //Send the say command
+    const embed = new BediEmbed()
+        .setTitle(say_title.value)
+        .setDescription(body_content_to_send);
+    return channel.send({embeds: [embed]});
+  }
+};
