a4da30a5cf071c0c394952cd53ac6e3c40a59626
Zayd Tahir <zaydtahir20@gmail.com>
2021-08-21 16:43:41 -0400

    Putting my name first cuz sahil thinks im special

--- src/lib/BediEmbed.ts
+++ src/lib/BediEmbed.ts
@@ -4,7 +4,7 @@ import colors from '../utils/colorUtil';
 export class BediEmbed extends MessageEmbed {
   public constructor() {
     super();
-    this.setFooter('For any concerns, contact a BediBot dev: Aadi, Carson, Sahil, & Zayd');
+    this.setFooter('For any concerns, contact a BediBot dev: Zayd, Aadi, Carson, Sahil');
     this.setTimestamp();
     this.setColor(colors.PRIMARY);
   }
