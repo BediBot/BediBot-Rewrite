b97b5ca1f733727dccd94dba1b6e9b630f653d48
Zayd Tahir <zaydtahir20@gmail.com>
2021-08-21 22:31:03 -0400

    Fix Quote Edge Cases (#136)
    * Update commands
    
    * Comment

--- src/commands/Quotes/getAllQuotes.ts
+++ src/commands/Quotes/getAllQuotes.ts
@@ -6,10 +6,12 @@ import {BediEmbed} from '../../lib/BediEmbed';
 import colors from '../../utils/colorUtil';
 import {getUserFromMention, surroundStringWithBackTick} from '../../utils/discordUtil';
 import {getSettings} from '../../database/models/SettingsModel';
+import {QUOTE_MAX_LENGTH} from './addQuote';
 
 const {Command} = require('@sapphire/framework');
 
 const MAX_QUOTES_PER_PAGE = 5;
+const EMBED_MAX_CHAR_LENGTH = 1024;
 
 module.exports = class GetAllQuotesCommand extends Command {
   constructor(context: PieceContext) {
@@ -62,8 +64,24 @@ module.exports = class GetAllQuotesCommand extends Command {
         if (quotes[i + j].date) title = quotes[i + j].date.toDateString();
         else title = 'Before Sep 2021';
 
-        if (user) field = `${surroundStringWithBackTick(quotes[i + j].quote)} by ${user.toString()}`;
-        else field = `${surroundStringWithBackTick(quotes[i + j].quote)} by ${surroundStringWithBackTick(quotes[i + j].name)}`;
+        let quoteText = quotes[i + j].quote;
+        if (quoteText.length > QUOTE_MAX_LENGTH) quoteText = quoteText.slice(QUOTE_MAX_LENGTH) + '...';
+
+        // If a quote contains a '<' then it probably contains a mention, so don't surround it with back ticks
+        if (quoteText.includes('<')) field = `${quoteText} by`;
+        else field = `${surroundStringWithBackTick(quoteText)} by`;
+
+        if (user) field += `${user.toString()}`;
+        else {
+          let name = quotes[i + j].name;
+
+          // Subtract 2 because of 2 characters from back ticks
+          if (name.length > (EMBED_MAX_CHAR_LENGTH - field.length - 2)) name.slice(EMBED_MAX_CHAR_LENGTH - field.length - 2);
+          field += `${surroundStringWithBackTick(quotes[i + j].name)}`;
+        }
+
+        // Just in case the field length is somehow too big, this will prevent an error from stopping the command from working
+        if (field.length > EMBED_MAX_CHAR_LENGTH) continue;
 
         embed.addField(title, field);
       }
--- src/commands/Quotes/getRandomQuote.ts
+++ src/commands/Quotes/getRandomQuote.ts
@@ -44,22 +44,27 @@ module.exports = class GetRandomQuoteCommand extends Command {
     const embed = new BediEmbed()
         .setTitle('Get Random Quote Reply');
 
+    let quoteText = quoteDoc.quote;
+
+    // If a quote contains a '<' then it probably contains a mention, so don't surround it with back ticks
+    if (!quoteText.includes('<')) quoteText = surroundStringWithBackTick(quoteText);
+
     if (quoteDoc.date) {
       if (typeof quoteAuthor.value === 'string') {
-        embed.setDescription(`Quote: ${surroundStringWithBackTick(quoteDoc.quote)}
+        embed.setDescription(`Quote: ${quoteText}
         Author: ${surroundStringWithBackTick(quoteDoc.name)}
         Date: ${surroundStringWithBackTick(quoteDoc.date.toDateString())}`);
       } else {
-        embed.setDescription(`Quote: ${surroundStringWithBackTick(quoteDoc.quote)}
+        embed.setDescription(`Quote: ${quoteText}
         Author: ${quoteDoc.name}
         Date: ${surroundStringWithBackTick(quoteDoc.date.toDateString())}`);
       }
     } else {
       if (typeof quoteAuthor.value === 'string') {
-        embed.setDescription(`Quote: ${surroundStringWithBackTick(quoteDoc.quote)}
+        embed.setDescription(`Quote: ${quoteText}
         Author: ${surroundStringWithBackTick(quoteDoc.name)}`);
       } else {
-        embed.setDescription(`Quote: ${surroundStringWithBackTick(quoteDoc.quote)}
+        embed.setDescription(`Quote: ${quoteText}
         Author: ${quoteDoc.name}`);
       }
     }
