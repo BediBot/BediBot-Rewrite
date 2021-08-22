95d22889b08d31cd23bdd606d0cf74a1ba8140c3
Zayd Tahir <zaydtahir20@gmail.com>
2021-08-21 21:30:34 -0400

    Add settings changing commands (#131)

---
+++ src/commands/Settings/setPrefix.ts
@@ -0,0 +1,43 @@
+import {Args, PieceContext} from '@sapphire/framework';
+import {Message} from 'discord.js';
+import {BediEmbed} from '../../lib/BediEmbed';
+import colors from '../../utils/colorUtil';
+import {surroundStringWithBackTick} from '../../utils/discordUtil';
+import settingsModel, {getSettings} from '../../database/models/SettingsModel';
+
+const {Command} = require('@sapphire/framework');
+
+module.exports = class SetPrefixCommand extends Command {
+  constructor(context: PieceContext) {
+    super(context, {
+      name: 'setPrefix',
+      aliases: ['sp'],
+      description: 'Changes the prefix for BediBot',
+      preconditions: ['GuildOnly', ['AdminOnly', 'BotOwnerOnly']],
+      detailedDescription: `${'setPrefix <newPrefix>`'}`,
+    });
+  }
+
+  async run(message: Message, args: Args) {
+    const {guildId} = message;
+    const settingsData = await getSettings(guildId as string);
+
+    // Check if they even inputted a string
+    const newValue = await args.peekResult('string');
+    if (!newValue.success) {
+      const embed = new BediEmbed()
+          .setColor(colors.ERROR)
+          .setTitle('Set Prefix Reply')
+          .setDescription(`Invalid Syntax!\n\nMake sure your command is in the format 
+          ${surroundStringWithBackTick(settingsData.prefix + 'setPrefix <newPrefix>')}`);
+      return message.reply({embeds: [embed]});
+    }
+
+    await settingsModel.updateOne({_id: guildId as string}, {prefix: newValue.value});
+
+    const embed = new BediEmbed()
+        .setTitle('Set Prefix Reply')
+        .setDescription(`The prefix has been updated to ${surroundStringWithBackTick(newValue.value)}`);
+    return message.reply({embeds: [embed]});
+  };
+};
---
+++ src/commands/Settings/setVerifiedRole.ts
@@ -0,0 +1,43 @@
+import {Args, PieceContext} from '@sapphire/framework';
+import {Message} from 'discord.js';
+import {BediEmbed} from '../../lib/BediEmbed';
+import colors from '../../utils/colorUtil';
+import {surroundStringWithBackTick} from '../../utils/discordUtil';
+import settingsModel, {getSettings} from '../../database/models/SettingsModel';
+
+const {Command} = require('@sapphire/framework');
+
+module.exports = class SetVerifiedRoleCommand extends Command {
+  constructor(context: PieceContext) {
+    super(context, {
+      name: 'setVerifiedRole',
+      aliases: ['svr'],
+      description: 'Changes the verified role for BediBot',
+      preconditions: ['GuildOnly', ['AdminOnly', 'BotOwnerOnly']],
+      detailedDescription: `${'setVerifiedRole <newRole>`'}`,
+    });
+  }
+
+  async run(message: Message, args: Args) {
+    const {guildId} = message;
+    const settingsData = await getSettings(guildId as string);
+
+    // Check if they even inputted a string
+    const newValue = await args.peekResult('role');
+    if (!newValue.success) {
+      const embed = new BediEmbed()
+          .setColor(colors.ERROR)
+          .setTitle('Set Verified Role Reply')
+          .setDescription(`Invalid Syntax!\n\nMake sure your command is in the format 
+          ${surroundStringWithBackTick(settingsData.prefix + 'setVerifiedRole <newRole>')}`);
+      return message.reply({embeds: [embed]});
+    }
+
+    await settingsModel.updateOne({_id: guildId as string}, {verifiedRole: newValue.value.name});
+
+    const embed = new BediEmbed()
+        .setTitle('Set Verified Role Reply')
+        .setDescription(`The verified role has been updated to ${surroundStringWithBackTick(newValue.value.name)}`);
+    return message.reply({embeds: [embed]});
+  };
+};
