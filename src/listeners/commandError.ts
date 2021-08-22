222d059da5045a03aa4be08660713b0f363fcaae
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-08-22 15:56:48 -0400

    Update logging properties for debugging (#150)

--- src/listeners/commandError.ts
+++ src/listeners/commandError.ts
@@ -11,7 +11,9 @@ module.exports = class CommandError extends Listener {
 
   public async run({context, message: content}: UserError, {message, command}: CommandErrorPayload) {
     const commandName = capFirstLetter(command.name);
-
     logger.error('Command Error:' + commandName + ' - ' + content);
+    logger.error('==== ERROR CONTEXT BEGIN ====');
+    logger.error(JSON.stringify(message)); //Log the message to ensure that we can debug later
+    logger.error('==== ERROR CONTEXT END ====');
   }
 };
