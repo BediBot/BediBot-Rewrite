74eb45d6e4d257c9e8c66ecd37925317dbce5e50
Zayd Tahir <zaydtahir20@gmail.com>
2022-01-03 10:02:55 -0500

    Formatting

--- src/commands/Settings/setEmailDomain.ts
+++ src/commands/Settings/setEmailDomain.ts
@@ -26,28 +26,28 @@ module.exports = class SetEmailDomainCommand extends Command {
         const newValue = await args.peekResult('string');
         if (!newValue.success) {
             const embed = new BediEmbed()
-                .setColor(colors.ERROR)
-                .setTitle('Set Email Domain Reply')
-                .setDescription(
-                    `Invalid Syntax!\n\nMake sure your command is in the format ` +
-                    `${Formatters.inlineCode(settingsData.prefix + 'setEmailDomain <domain>')}`);
+                              .setColor(colors.ERROR)
+                              .setTitle('Set Email Domain Reply')
+                              .setDescription(
+                                  `Invalid Syntax!\n\nMake sure your command is in the format ` +
+                                  `${Formatters.inlineCode(settingsData.prefix + 'setEmailDomain <domain>')}`);
             return message.reply({embeds: [embed]});
         }
 
         if (newValue.value.startsWith('@')) {
             const embed = new BediEmbed()
-                .setColor(colors.ERROR)
-                .setTitle('Set Email Domain Reply')
-                .setDescription('Your email domain should not include the @ symbol!');
+                              .setColor(colors.ERROR)
+                              .setTitle('Set Email Domain Reply')
+                              .setDescription('Your email domain should not include the @ symbol!');
             return message.reply({embeds: [embed]});
         }
 
         await settingsModel.updateOne({_id: guildId as string}, {emailDomain: newValue.value});
 
         const embed = new BediEmbed()
-            .setTitle('Set Email Domain Reply')
-            .setColor(colors.SUCCESS)
-            .setDescription(`The email domain has been updated to ${Formatters.inlineCode(newValue.value)}`);
+                          .setTitle('Set Email Domain Reply')
+                          .setColor(colors.SUCCESS)
+                          .setDescription(`The email domain has been updated to ${Formatters.inlineCode(newValue.value)}`);
         return message.reply({embeds: [embed]});
     };
 };
